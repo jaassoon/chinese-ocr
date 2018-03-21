36aeff93aa45a0978d95e210dbd7c30b444cf292
jason <linglijunmail@gmail.com>
2018-03-21 15:33:50 +0900

    update tel utils

--- receiptUtils/telUtils.py
+++ receiptUtils/telUtils.py
@@ -1,14 +1,16 @@
 #coding:utf-8
+from receiptUtils import numberUtils
+
 def getTel(tmpResult,resultMap):
     print('input tel {}'.format(tmpResult))
     tmpResult=tmpResult.split(' ')[-1]
     tmpResult=tmpResult.replace('-','')
     if(tmpResult.find(':') > 0):
         tmpResult=tmpResult.split(':')[-1]
-    tmpResult = tmpResult.replace('l','1').replace('A','4')
-    tmpResult = tmpResult.replace('月','9').replace(']','1')
-    tmpResult = tmpResult.replace(':','').replace('g','9')
-    tmpResult = tmpResult.replace('e','6')
+    tmpResult=tmpResult.replace(' ','')
+    if(len(tmpResult)>10):
+        tmpResult=tmpResult[-10:-1]
+    tmpResult = numberUtils.numberReplacement(tmpResult)
     resultMap['3_tel'] = tmpResult
     print('output tel {}'.format(tmpResult))
     return resultMap
