e8e86bd191271dd5db5f4cc715c145fdbb93f7c9
jason <linglijunmail@gmail.com>
2018-03-23 22:31:21 +0900

    update number utils

--- receiptUtils/numberUtils.py
+++ receiptUtils/numberUtils.py
@@ -11,7 +11,9 @@ def numberReplacement(tmpResult):
       .replace('J', '1') \
       .replace('Z', '1') \
       .replace('?', '2') \
+      .replace('z', '2') \
       .replace('A', '4')\
+      .replace('F', '5') \
       .replace('S', '5') \
       .replace('e', '6') \
       .replace('B', '8')\
@@ -23,5 +25,6 @@ def numberReplacement(tmpResult):
       .replace(':', '')\
       .replace(',', '')\
       .replace('，', '')\
+      .replace('、', '')\
       .replace(' ','')
   return tmpResult
