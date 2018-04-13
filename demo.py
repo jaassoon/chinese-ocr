35aadd093b668707631738eab16486c2b47c2f90
jason <linglijunmail@gmail.com>
2018-04-14 07:26:55 +0900

    add draw_boxes into commonutils

--- demo.py
+++ demo.py
@@ -3,8 +3,14 @@ import model
 from glob import glob
 import numpy as np
 from PIL import Image
-import time,sys
+import time,argparse
 im_names = glob('./test/*.jpg')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--develop', action='store_true', help='to gen result img')
+parser.add_argument('--torch', action='store_true', help='to use torch model')
+
+opt = parser.parse_args()
 
 def printOriginResult(result):
     with open('./' + 'result_origin.txt', 'a+') as f:
@@ -12,18 +18,14 @@ def printOriginResult(result):
         for key in result:
             strResult += '{}\r\n'.format(result[key][1])
         f.write(strResult)
+
 if __name__ =='__main__':
-    lineNo=0
-    devFlg=False
-    if (len(sys.argv) > 1):
-        devFlg=True
     for im_name in im_names:
       im = Image.open(im_name)
       img = np.array(im.convert('RGB'))
       t = time.time()
       print(im_name + "---------------------------------------")
-      # result,img,angle,lineNo = model.model(img,lineNo,model='torch',im_name=im_name)
-      result,img,angle,lineNo = model.model(img,lineNo,devFlg,model='keras',im_name=im_name)
+      result,img,angle = model.model(img,opt,model='keras',im_name=im_name)
       print("It takes time:{}s".format(time.time() - t))
-      if(devFlg):
-          printOriginResult(result)
+      if opt.develop:
+        printOriginResult(result)
--- receiptUtils/commonUtils.py
+++ receiptUtils/commonUtils.py
@@ -1,7 +1,9 @@
 #coding:utf-8
 from receiptUtils import telUtils, staffNoUtils,shopNameUtils,commonUtils,\
 timeUtils, cityUtils,priceUtils,cardUtils,categoryUtils,taxUtils,numberUtils
-import re
+import os,cv2
+import numpy as np
+from PIL import Image
 
 def adjustPredict(i,sim_pred,resultMap,pos_tax):
     # ---------------------------------subtotal
@@ -42,3 +44,71 @@ def getDrawboxResult(origin_result,resultMap):
     staffNoUtils.getNo(origin_result[pos_ling+2],resultMap,pos_ling+2)#match family
     for i, sim_pred in enumerate(origin_result):
         commonUtils.adjustPredict(i,sim_pred,resultMap,pos_tax)
+
+def draw_boxes(img,image_name,boxes,opt,adjust):
+    base_name = image_name.split('/')[-1]
+    i=0
+    j=0
+    boxes = sorted(boxes, key=lambda x: x[1])
+    boxesX0 = sorted(boxes, key=lambda x: x[0])
+    xStart=boxesX0[0][0]
+    iXStart=int(xStart)
+    if(iXStart<0):
+        iXStart=0
+    boxesX2 = sorted(boxes, key=lambda x: x[2])
+    xEnd=boxesX2[-1][2]
+    previousY=0
+    resultMap={'1_shopName':'ファミア!!','2_city':'none',
+              '3_tel':'1234567890','4_year':'none',
+              '5_total':0,
+              '6_receiptNO':'none',
+              '7_staffNO':'none',
+              '8_pointcard':'none',
+              '9_category':0,
+              'a_goods':'none',
+
+              'a_tax':0,
+              'b_subtotal':0,
+              'suffix_catPrice':[],
+              'pos_tel_after':0,
+              'origin_result':[],
+              'pos_shop':0,
+              'pos_time':0,
+              'pos_time_after':0,
+              'pos_tax_after':0,
+              'pos_card_after':0,
+              'pos_staff':0,
+              'pos_ling':0,
+              'pos_category':0,
+              'pos_total':0,
+              'pos_subtotal':0,
+              'pos_tax':0,
+              'pos_card':0,
+              'type_shop':0}
+    for box in boxes:
+        if np.linalg.norm(box[0] - box[1]) < 5 or np.linalg.norm(box[3] - box[0]) < 5:
+            j+=1
+            continue
+        if(i>0 and int(box[1])-previousY<=11):
+            previousY=int(box[1])
+            continue
+        previousY = int(box[1])
+        i+=1
+        crop_img = img[int(box[1]):int(box[5]), iXStart:int(xEnd)]
+        # print('Y0={}, Y1={}, X0={}, X1={}'.format(int(box[1]),int(box[5]),iXStart,int(xEnd)))
+        if opt.develop:
+          cv2.imwrite(os.path.join("test/results", base_name.split('.')[0] + "_" + str(i) + ".jpg"), crop_img)
+        try:
+            if opt.torch:
+              from crnn.crnn import crnnOcr
+              sim_pred = crnnOcr(Image.fromarray(crop_img ).convert('L')).strip()
+            else:
+              from ocr.model import predict as ocr
+              sim_pred = ocr(Image.fromarray(crop_img ).convert('L')).strip()
+        except Exception as e:
+            print('Exception for step {}--{}'.format(i,e))
+            continue
+        sim_pred=sim_pred.strip()
+        resultMap['origin_result'].append(sim_pred)
+    commonUtils.getDrawboxResult(resultMap['origin_result'], resultMap)
+    return resultMap
