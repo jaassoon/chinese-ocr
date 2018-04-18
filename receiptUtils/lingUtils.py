563cbf1406645d635bf4bc0bed838a5911b0f15d
jason <linglijunmail@gmail.com>
2018-04-18 21:57:53 +0900

    add ling parser

---
+++ receiptUtils/lingUtils.py
@@ -0,0 +1,15 @@
+#coding:utf-8
+from receiptUtils import numberUtils
+import re
+
+def getPosLing(resultMap,result):
+  for i in result:
+      if result[i][1].find('領 証') > -1 \
+          or result[i][1].find('領収') > -1 \
+          or result[i][1].find('収証') > -1 \
+          or result[i][1].find('領 書') > -1 \
+          or result[i][1].find('収書') > -1:
+          resultMap['pos_ling_after'] = i
+
+  if(resultMap['pos_ling_after']>0):
+    print('output pos_ling_after------------- {}'.format(resultMap['pos_ling_after']))
